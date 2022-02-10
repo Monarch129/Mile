d4668983fb6170401ffa1730f55ac2c7a7307d51
Monarch129 <antipo97@mail.ru>
2022-02-10 14:46:21 +0300

    origin/master

--- src/Main.java
+++ src/Main.java
@@ -2,8 +2,8 @@ public class Main {
 
 
     public static void main(String[] args) {
-        int ticketcoast = 200;
-        int mile = ticketcoast / 20;
+        int TicketCoast = 200;
+        int mile = TicketCoast / 20;
         System.out.println("Бонусные мили за покупку билета: " + mile);
     }
 }
